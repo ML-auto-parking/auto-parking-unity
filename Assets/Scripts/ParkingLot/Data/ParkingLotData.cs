a2f23f9c631ba367351db51c3fa52c90f03d5f72
DAY0522 <ekdud_____@naver.com>
2024-05-04 20:14:05 +0900

    feat: 주차 공간 초기화 구현
    - 평행, 수직 공간 각각 1개씩 비워지도록 구현

--- Assets/Scripts/ParkingLot/Data/ParkingLotData.cs
+++ Assets/Scripts/ParkingLot/Data/ParkingLotData.cs
@@ -3,19 +3,47 @@ using UnityEngine;
 
 namespace AutonomousParking.ParkingLot.Data
 {
+    // ParkingLotData 클래스는 주차장 데이터를 관리하는 컴포넌트입니다.
     public class ParkingLotData : MonoBehaviour
     {
-        [field: SerializeField, Min(default)] public int FreeParkingSpotsCount { get; private set; }
+        // 주차 공간에 대한 데이터를 저장하는 ParkingSpotData 객체입니다.
         [field: SerializeField] public ParkingSpotData ParkingSpotData { get; private set; }
-        [field: SerializeField] public List<Transform> AvailableParkingSpots { get; private set; }
+
+        // 사용 가능한 주차 공간들의 Transform 컴포넌트 목록입니다.
+        public List<Transform> AvailableParkingSpots { get; private set; }
+        [field: SerializeField] public List<Transform> AvailableHorizontalParkingSpots { get; private set; }
+        [field: SerializeField] public List<Transform> AvailableVerticalParkingSpots { get; private set; }
+
+
+        // 현재 사용 가능한 주차 공간들의 목록입니다. 
         public List<Transform> CurrentlyAvailableParkingSpots { get; private set; }
+        public List<Transform> CurrentlyAvailableHorizontalParkingSpots { get; private set; }
+        public List<Transform> CurrentlyAvailableVerticalParkingSpots { get; private set; }
 
-        private void Awake() => CurrentlyAvailableParkingSpots = new List<Transform>(AvailableParkingSpots);
+        // Awake 메소드는 객체가 처음 활성화될 때 호출됩니다. 이 메소드는 AvailableParkingSpots의 복사본을 CurrentlyAvailableParkingSpots로 설정합니다.
+        private void Awake(){
+            AvailableParkingSpots = new List<Transform>();
+            AvailableParkingSpots.AddRange(AvailableHorizontalParkingSpots);
+            AvailableParkingSpots.AddRange(AvailableVerticalParkingSpots);
 
+            CurrentlyAvailableHorizontalParkingSpots = new List<Transform>(AvailableHorizontalParkingSpots);
+            CurrentlyAvailableVerticalParkingSpots = new List<Transform>(AvailableVerticalParkingSpots);
+
+            CurrentlyAvailableParkingSpots = new List<Transform>();
+            // CurrentlyAvailableHorizontalParkingSpots의 모든 요소를 CurrentlyAvailableParkingSpots에 추가합니다.
+            CurrentlyAvailableParkingSpots.AddRange(CurrentlyAvailableHorizontalParkingSpots);
+            // CurrentlyAvailableVerticalParkingSpots의 모든 요소를 CurrentlyAvailableParkingSpots에 추가합니다.
+            CurrentlyAvailableParkingSpots.AddRange(CurrentlyAvailableVerticalParkingSpots);
+        }
+
+        // Reset 메소드는 주차장의 상태를 초기화합니다. 모든 주차 공간을 다시 사용 가능하게 설정합니다.
         public void Reset()
         {
-            CurrentlyAvailableParkingSpots.Clear();
-            CurrentlyAvailableParkingSpots.AddRange(AvailableParkingSpots);
+            CurrentlyAvailableHorizontalParkingSpots.Clear();  // 현재 목록을 비웁니다.
+            CurrentlyAvailableVerticalParkingSpots.Clear();  // 현재 목록을 비웁니다.
+
+            CurrentlyAvailableHorizontalParkingSpots.AddRange(AvailableHorizontalParkingSpots);
+            CurrentlyAvailableVerticalParkingSpots.AddRange(AvailableVerticalParkingSpots);
         }
     }
 }
--- Assets/Scripts/ParkingLot/ObjectPlacers/ParkingLotParkedCarsPlacer.cs
+++ Assets/Scripts/ParkingLot/ObjectPlacers/ParkingLotParkedCarsPlacer.cs
@@ -6,27 +6,46 @@ using UnityEngine;
 
 namespace AutonomousParking.ParkingLot.ObjectPlacers
 {
+    // ParkingLotParkedCarsPlacer 클래스는 주차장에 자동차를 배치하는 기능을 담당합니다.
     public class ParkingLotParkedCarsPlacer : MonoBehaviour
     {
+        // CarSpawner 객체에 대한 참조를 직렬화하여 인스펙터에서 설정할 수 있도록 합니다.
         [SerializeField] private CarSpawner carSpawner;
+        // ParkingLotData 객체에 대한 참조를 직렬화하여 인스펙터에서 설정할 수 있도록 합니다.
         [SerializeField] private ParkingLotData parkingLotData;
 
+        // Place 메소드는 주차장에 차량을 배치하는 로직을 수행합니다.
         public void Place()
         {
+            // 랜덤으로 차를 배치할 주차 공간을 선택합니다.
             List<Transform> parkingSpotsToOccupy = PickRandomParkingSpotsToOccupy();
+            // 각 주차 공간에 차량을 배치합니다.
             parkingSpotsToOccupy.ForEach(parkingSpot => carSpawner.Spawn(parkingSpot, parkingLotData.ParkingSpotData));
 
+            // PickRandomParkingSpotsToOccupy 메소드는 무작위로 차량을 배치할 주차 공간을 결정합니다.
             List<Transform> PickRandomParkingSpotsToOccupy()
             {
-                List<Transform> availableParkingSpots = parkingLotData.CurrentlyAvailableParkingSpots;
-                int occupiedParkingSpotsCount = availableParkingSpots.Count - parkingLotData.FreeParkingSpotsCount;
-                return availableParkingSpots.ExtractRandomItems(occupiedParkingSpotsCount);
+                // 현재 사용 가능한 주차 공간 목록을 가져옵니다.
+                List<Transform> availableVerticalParkingSpots = parkingLotData.CurrentlyAvailableVerticalParkingSpots;
+                List<Transform> availableHorizontalParkingSpots = parkingLotData.CurrentlyAvailableHorizontalParkingSpots;
+                // 차량이 배치될 주차 공간 수를 계산합니다.
+                int occupiedVerticalParkingSpotsCount = availableVerticalParkingSpots.Count - 1;
+                int occupiedHorizontalParkingSpotsCount = availableHorizontalParkingSpots.Count - 1;
+                
+                List<Transform> totalParkingSpotsToOccupy = availableVerticalParkingSpots.ExtractRandomItems(occupiedVerticalParkingSpotsCount);
+                totalParkingSpotsToOccupy.AddRange(availableHorizontalParkingSpots.ExtractRandomItems(occupiedHorizontalParkingSpotsCount));
+
+                // 랜덤으로 주차 공간을 추출합니다.
+                return totalParkingSpotsToOccupy;
             }
         }
 
+        // Remove 메소드는 모든 차량을 주차장에서 제거하고 주차장 데이터를 리셋합니다.
         public void Remove()
         {
+            // CarSpawner를 통해 모든 차량을 제거합니다.
             carSpawner.DeSpawnAll();
+            // 주차장 데이터를 초기 상태로 복원합니다.
             parkingLotData.Reset();
         }
     }
